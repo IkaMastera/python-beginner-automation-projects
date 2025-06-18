d02e2114de9a65e8005d1be3236863a693f86203
Irakli <112602982+IkaMastera@users.noreply.github.com>
2025-06-18 13:10:59 +0400

    add: all the imports and file destinations

---
+++ backup.py
@@ -0,0 +1,8 @@
+import os
+import shutil
+import datetime
+import schedule
+import time
+
+source_dir = "C:\Users\irobo\OneDrive\Pictures\Screenshots"
+destination_dir = "C:\Users\irobo\Documents\Backups"
