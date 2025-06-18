e2083607f1e96fd81238b83f820c98fc2c4e0c9c
Irakli <112602982+IkaMastera@users.noreply.github.com>
2025-06-18 13:16:55 +0400

    add: static version of the application

--- backup.py
+++ backup.py
@@ -4,5 +4,17 @@ import datetime
 import schedule
 import time
 
-source_dir = "C:\Users\irobo\OneDrive\Pictures\Screenshots"
-destination_dir = "C:\Users\irobo\Documents\Backups"
+source_dir = "C:/Users/irobo/OneDrive/Pictures/Screenshots"
+destination_dir = "C:/Users/irobo/Documents/Backups"
+
+def copy_folder_to_directory(source, dest):
+    today = datetime.date.today()
+    dest_dir = os.path.join(dest, str(today))
+
+    try:
+        shutil.copytree(source, dest_dir)
+        print(f"Folder copied to: {dest_dir}")
+    except FileExistsError:
+        print(f"Folder already exists in: {dest}")
+
+copy_folder_to_directory(source_dir, destination_dir)
